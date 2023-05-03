ba11be2f0b33b8c5ef93bbe3bd3b5f1922e9e998
Lovakovic <lovakovdario@gmail.com>
2023-05-03 20:28:05 +0200

    Implement view of the courses student is enrolled in

--- frontend/src/app/pages/details/details.component.ts
+++ frontend/src/app/pages/details/details.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import Student from "../../model/student.model";
 import {ActivatedRoute} from "@angular/router";
 import {StudentService} from "../../service/student.service";
+import {Observable} from "rxjs";
+import Course from "../../model/course.model";
+import {CourseService} from "../../service/course.service";
 
 @Component({
   selector: 'app-details',
@@ -9,16 +12,21 @@ import {StudentService} from "../../service/student.service";
 })
 export class DetailsComponent implements OnInit {
   student?: Student;
+  courses$?: Observable<Course[]>;
   editMode: boolean = false;
 
   constructor(private route: ActivatedRoute,
-              private studentService: StudentService) {}
+              private studentService: StudentService,
+              private courseService: CourseService
+  ) {}
 
   ngOnInit(): void {
     let studentJmbag = this.route.snapshot.paramMap.get('jmbag') as string;
     this.studentService.getStudentByJmbag(studentJmbag).subscribe(_student => {
       this.student = _student;
     })
+
+    this.courses$ = this.courseService.getCoursesByStudentJmbag(studentJmbag);
   }
 
   updateStudentInfo(updatedStudent: Student): void {
