3b960cd2d9ebc7af8edbeda75a29f8860d166031
Lovakovic <lovakovdario@gmail.com>
2023-05-14 19:22:25 +0200

    Adapt insert statements to new `student` entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,13 +1,13 @@
 -- Students
-INSERT INTO student (jmbag, firstName, lastName, dateOfBirth, ectsPoints, enrolledStudiesAtYear, currentSemester, email, phone, major)
+INSERT INTO student (jmbag, firstName, lastName, dateOfBirth, ectsPoints)
 VALUES
-    ('9283758271', 'Ivan', 'Markovic', '2000-01-01', 60, 2019, 4, 'ivan.markovic@student.com', '+38591234567', 'Computer Science'),
-    ('0284729172', 'Horvat', 'Horvatinic', '2001-02-02', 78, 2021, 3, 'horvat.horvatinic@student.com', '+38598765432', 'Business Administration'),
-    ('7261948261', 'Ana', 'Babic', '2002-03-05', 72, 2020, 2, 'ana.babic@student.com', '+38595544333', 'Electrical Engineering'),
-    ('8826183962', 'Petar', 'Novak', '2000-05-10', 85, 2019, 4, 'petar.novak@student.com', '+38591666666', 'Mechanical Engineering'),
-    ('5719273628', 'Ivana', 'Kovac', '1999-09-20', 90, 2018, 2, 'ivana.kovac@student.com', '+38597777777', 'Mathematics'),
-    ('2957281941', 'Luka', 'Hodak', '2003-04-15', 65, 2022, 1, 'luka.hodak@student.com', '+38595555555', 'Environmental Science'),
-    ('4791826392', 'Jana', 'Bogdan', '2001-07-25', 80, 2021, 2, 'jana.bogdan@student.com', '+38594444444', 'Architecture');
+    ('9283758271', 'Ivan', 'Markovic', '2000-01-01', 60),
+    ('0284729172', 'Horvat', 'Horvatinic', '2001-02-02', 78),
+    ('7261948261', 'Ana', 'Babic', '2002-03-05', 72),
+    ('8826183962', 'Petar', 'Novak', '2000-05-10', 85),
+    ('5719273628', 'Ivana', 'Kovac', '1999-09-20', 90),
+    ('2957281941', 'Luka', 'Hodak', '2003-04-15', 65),
+    ('4791826392', 'Jana', 'Bogdan', '2001-07-25', 80);
 
 -- Courses
 INSERT INTO course (name, ects)
