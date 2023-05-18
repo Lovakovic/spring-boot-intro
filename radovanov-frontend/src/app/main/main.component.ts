50bcde5033da82e62e2a7d4600f6668049cd324f
Lovakovic <lovakovdario@gmail.com>
2023-05-18 18:14:44 +0200

    Hide `Logins` button in case user doesn't have sufficient privileges

--- radovanov-frontend/src/app/main/main.component.ts
+++ radovanov-frontend/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../user/user.service';
 
 @Component({
   selector: 'app-main',
@@ -9,7 +10,7 @@ export class MainComponent implements OnInit {
 
   title = 'Student App';
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
   }
