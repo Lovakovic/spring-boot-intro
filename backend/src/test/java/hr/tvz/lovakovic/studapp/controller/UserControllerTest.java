6f9801afea436547a2486a334fa749583283b1a7
Lovakovic <lovakovdario@gmail.com>
2023-06-01 17:20:52 +0200

    Create `UserControllerTest`

---
+++ backend/src/test/java/hr/tvz/lovakovic/studapp/controller/UserControllerTest.java
@@ -0,0 +1,79 @@
+package hr.tvz.lovakovic.studapp.controller;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hr.tvz.lovakovic.studapp.model.UserDTO;
+import hr.tvz.lovakovic.studapp.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Optional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+        // Mock authentication
+        UserDetails principal = User.withUsername("test")
+                .password("password")
+                .authorities(AuthorityUtils.createAuthorityList("ROLE_USER"))
+                .build();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    @DirtiesContext
+    @Test
+    public void getCurrentUserTest() throws Exception {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        userDTO.setUsername("test");
+        userDTO.setFirstName("Test");
+        userDTO.setLastName("User");
+
+        when(userService.findByUsername(anyString())).thenReturn(Optional.of(userDTO));
+
+        mockMvc.perform(get("/api/user/current-user")
+                        .with(user("test"))  // pass the user to the request
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
