2a79233c7c8d9a6095abc672efd5e6473df694a7
Lovakovic <lovakovdario@gmail.com>
2023-06-03 20:48:38 +0200

    Remove unused imports

--- backend/src/main/java/hr/tvz/lovakovic/studapp/mapper/UserMapper.java
+++ backend/src/main/java/hr/tvz/lovakovic/studapp/mapper/UserMapper.java
@@ -3,8 +3,6 @@ package hr.tvz.lovakovic.studapp.mapper;
 import hr.tvz.lovakovic.studapp.model.User;
 import hr.tvz.lovakovic.studapp.model.UserDTO;
 
-import java.util.stream.Collectors;
-
 public class UserMapper {
     public static UserDTO toDTO(User user) {
         UserDTO userDTO= new UserDTO();
--- backend/src/main/java/hr/tvz/lovakovic/studapp/model/UserDTO.java
+++ backend/src/main/java/hr/tvz/lovakovic/studapp/model/UserDTO.java
@@ -2,8 +2,6 @@ package hr.tvz.lovakovic.studapp.model;
 
 import lombok.Data;
 
-import java.util.Set;
-
 @Data
 public class UserDTO {
     private Long id;
