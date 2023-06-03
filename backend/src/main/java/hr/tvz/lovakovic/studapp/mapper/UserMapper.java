ebba879256cefa161323ecf26a8ce2319978e4de
Lovakovic <lovakovdario@gmail.com>
2023-06-03 20:46:28 +0200

    Replace `Authority` model with `Role`, replace set of authorities with single `Role` attribute

--- backend/src/main/java/hr/tvz/lovakovic/studapp/mapper/UserMapper.java
+++ backend/src/main/java/hr/tvz/lovakovic/studapp/mapper/UserMapper.java
@@ -1,6 +1,5 @@
 package hr.tvz.lovakovic.studapp.mapper;
 
-import hr.tvz.lovakovic.studapp.model.Authority;
 import hr.tvz.lovakovic.studapp.model.User;
 import hr.tvz.lovakovic.studapp.model.UserDTO;
 
@@ -14,9 +13,7 @@ public class UserMapper {
         userDTO.setUsername(user.getUsername());
         userDTO.setFirstName(user.getFirstName());
         userDTO.setLastName(user.getLastName());
-        userDTO.setAuthorities(user.getAuthorities().stream()
-                .map(Authority::getName)
-                .collect(Collectors.toSet()));
+        userDTO.setRole(String.valueOf(user.getRole()));
 
         return userDTO;
     }
