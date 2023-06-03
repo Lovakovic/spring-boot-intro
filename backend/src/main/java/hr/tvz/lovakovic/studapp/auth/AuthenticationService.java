3d43ce2db3dde48bf8c48b1250efd94756daae47
Lovakovic <lovakovdario@gmail.com>
2023-06-03 20:41:47 +0200

    Create new auth classes for `AuthenticationController` and `AuthenticationService`

---
+++ backend/src/main/java/hr/tvz/lovakovic/studapp/auth/AuthenticationService.java
@@ -0,0 +1,56 @@
+package hr.tvz.lovakovic.studapp.auth;
+
+import hr.tvz.lovakovic.studapp.model.Role;
+import hr.tvz.lovakovic.studapp.model.User;
+import hr.tvz.lovakovic.studapp.repository.UserRepository;
+import hr.tvz.lovakovic.studapp.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    // Also record the login session as a LoginRecord
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ROLE_USER)
+                .build();
+
+        userRepository.save(user);
+        String jwt = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwt)
+                .build();
+    }
+
+    // Also record the login session as a LoginRecord
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        // User is authenticated, we can now generate token for that user
+        User user = userRepository.findOneByUsername(request.getUsername())
+                .orElseThrow();
+        String jwt = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwt)
+                .build();
+    }
+}
