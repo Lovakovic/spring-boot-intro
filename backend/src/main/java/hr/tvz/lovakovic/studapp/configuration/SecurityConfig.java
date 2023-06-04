3e86ea7b5cd7f2259f9e518ca549fa4f4d5b02d7
Lovakovic <lovakovdario@gmail.com>
2023-06-04 14:52:43 +0200

    Return `401` instead of `403` on unauthorized requests

--- backend/src/main/java/hr/tvz/lovakovic/studapp/configuration/SecurityConfig.java
+++ backend/src/main/java/hr/tvz/lovakovic/studapp/configuration/SecurityConfig.java
@@ -4,11 +4,13 @@ import hr.tvz.lovakovic.studapp.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -30,6 +32,9 @@ public class SecurityConfig {
                 )
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
