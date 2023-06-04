29844e8cd0cafb28c066ae88e415dda069561bac
Lovakovic <lovakovdario@gmail.com>
2023-06-04 15:26:22 +0200

    Remove unused exception

--- backend/src/main/java/hr/tvz/lovakovic/studapp/security/InvalidJwtAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package hr.tvz.lovakovic.studapp.security;
-
-public class InvalidJwtAuthenticationException extends RuntimeException {
-    public InvalidJwtAuthenticationException(String message) {
-        super(message);
-    }
-}
